e94a7c54e4bd4fe1c326597725148721f2fae9fc
Billy Fung <billy@billyfung.com>
2015-11-10 10:57:49 -0800

    using numpy as well

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 import tensorflow as tf 
+import numpy as np
 
 #on core2duo laptop, set cores to 2
 sess = tf.Session( config=tf.ConfigProto(inter_op_parallelism_threads=2,
@@ -9,7 +10,19 @@ hello = tf.constant('Hello World')
 
 print sess.run(hello)
 
-a = tf.constant(50)
-b = tf.constant(45)
+with sess:
+	input1 = tf.constant(1, shape = [4])
+	input2 = tf.constant(2, shape = [4])
+	output = (input1 + input2)
+	result = output.eval()
+	print result
 
-print sess.run(a*b)
+	input_features = tf.constant(np.reshape([1, 0, 0, 1], (1,4)).astype(np.float32))
+	weights = tf.constant(np.random.randn(4,2).astype(np.float32))
+	output = tf.matmul(input_features, weights) #matrix multiplication
+	print "Input:" 
+	print input_features.eval()
+	print "Weights:"
+	print weights.eval()
+	print "Output"
+	print output.eval()
